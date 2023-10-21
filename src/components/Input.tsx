2f0e427f1d1a17cd0aacec9ecc43567fdecda6e8
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2023-10-21 23:50:09 +0300

    login page

---
+++ src/components/Input.tsx
@@ -0,0 +1,31 @@
+import { ErrorMessage } from "formik";
+import React from "react";
+
+type InputType = {
+  name: string;
+  type?: string;
+  placeholder?: string;
+  onChange: any;
+  value: string;
+};
+
+const Input: React.FC<InputType> = ({ ...props }) => {
+  const { name, placeholder } = props;
+  return (
+    <div className="space-y-2">
+      <label id={name} className="text-xs px-2">
+        {placeholder}
+      </label>
+      <input
+        {...props}
+        autoComplete="off"
+        className="block w-full outline-none py-3 px-5 text-sm border border-1 border-gray-300 rounded-full bg-white focus:border-indigo-500"
+      />
+      <p className="text-xs px-5 text-red-500">
+        <ErrorMessage name={name} />
+      </p>
+    </div>
+  );
+};
+
+export default Input;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import * as yup from "yup";
+import Input from "../components/Input";
+
+const Login = () => {
+  return (
+    <div className="min-h-screen p-8 space-y-10 flex flex-col justify-center bg-white">
+      {/* <div className="text-center">
+        <p className="text-4xl font-semibold">Header</p>
+      </div> */}
+      <div className="space-y-5 justify-center w-full sm:w-4/6 md:w-1/2 lg:w-1/3 mx-auto">
+        <div className="space-y-3">
+          <h2 className="text-3xl font-semibold">Login</h2>
+          <p className="text-sm text-gray-500">Welcome back</p>
+        </div>
+        <Formik
+          initialValues={{
+            email: "",
+            password: "",
+          }}
+          onSubmit={(values) => {
+            console.log(values);
+          }}
+          validationSchema={loginValidation}
+        >
+          {({ values, errors, handleChange, handleSubmit }) => (
+            <Form className="space-y-4">
+              <Input
+                name="email"
+                value={values.email}
+                onChange={handleChange}
+                type="email"
+                placeholder="Email"
+              />
+
+              <Input
+                name="password"
+                value={values.password}
+                onChange={handleChange}
+                type="password"
+                placeholder="Password"
+              />
+              <button
+                type="submit"
+                className="text-sm block w-full bg-indigo-500 text-white py-3 rounded-full font-semibold"
+              >
+                Login
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
+const loginValidation = yup.object().shape({
+  email: yup.string().email("Invalid Email").required("Email is required"),
+  password: yup.string().required("Password is required"),
+});
