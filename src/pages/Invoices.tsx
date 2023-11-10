01af848b2598a138ca1949bb5ab2320bde51a3e9
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2023-11-10 10:14:59 +0300

    invoice listed

--- src/pages/Invoices.tsx
+++ src/pages/Invoices.tsx
@@ -1,9 +1,43 @@
 import React from "react";
+import Invoice from "../components/Invoice";
 
 const Invoices = () => {
+  const invoices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 10, 11, 12, 13, 14, 15];
   return (
-    <div>
-      <p>Invoices</p>
+    <div className="flex space-between w-full">
+      <div className="flex-1 space-y-5 md:pt-8 px-5">
+        {/* page heading */}
+        <div className="flex items-center justify-between py-3">
+          <p className="text-2xl">Invoices</p>
+        </div>
+        {/* Filters */}
+        <div></div>
+        {/* List of invoices */}
+        <div>
+          <div className="bg-gray-200 p-4 rounded-t-lg grid grid-cols-5 gap-4">
+            <div className="col-span-1">
+              <p>Issuance Date</p>
+            </div>
+            <div className="col-span-1">
+              <p>Validity Date</p>
+            </div>
+            <div className="col-span-1">
+              <p>Currency</p>
+            </div>
+            <div className="col-span-1">
+              <p>Amount</p>
+            </div>
+            <div className="col-span-1">
+              <p>Status</p>
+            </div>
+          </div>
+          {invoices.map((inv) => (
+            <div key={inv}>
+              <Invoice />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/utils/users.ts
@@ -0,0 +1,15 @@
+const sales = [
+  {
+    firstName: "Walker",
+    lastName: "Paul",
+    emailAddress: "walker@walker.com",
+    gender: "male",
+    contactNumber: "+893740230834",
+    phoneNumber: "+743823989047",
+    whatsappNumber: "+44872309032",
+    status: "active",
+    role: "sales",
+  },
+];
+
+export default sales;
