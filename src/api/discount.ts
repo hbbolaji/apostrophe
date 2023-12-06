5aee9c69db15d2f69effd049a8d9741587c48509
HASHIM BOLAJI BELLO <bolaji1050@gmail.com>
2023-12-06 16:21:15 +0300

    Edit discount loading and error feature

--- src/api/discount.ts
+++ src/api/discount.ts
@@ -34,3 +34,25 @@ export const createDiscount = async (token: string, values: FormData) => {
     return error;
   }
 };
+
+export const editDiscount = async (
+  token: string,
+  id: string,
+  values: FormData
+) => {
+  try {
+    const result = await axios.patch(
+      `${process.env.REACT_APP_BASE_URL}/api/v1/discount/scheme/${id}`,
+      values,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
--- src/pages/EditDiscount.tsx
+++ src/pages/EditDiscount.tsx
@@ -1,34 +1,37 @@
 import { Form, Formik } from "formik";
-import React from "react";
+import React, { useState } from "react";
+import * as yup from "yup";
 import { getFormData } from "../utils/helper";
 import Input from "../components/Input";
 import { useAuth } from "../context/AuthContext";
-import axios from "axios";
 import { useLocation, useNavigate } from "react-router-dom";
+import { editDiscount } from "../api/discount";
+import ButtonSpinner from "../components/ButtonSpinner";
+import Toast from "../components/Toast";
 
 const EditDiscount = () => {
   const { state } = useLocation();
   const { token } = useAuth();
+
+  const [error, setError] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
   const navigate = useNavigate();
-  const EditDiscount = async (values: FormData) => {
-    try {
-      await axios.patch(
-        `${process.env.REACT_APP_BASE_URL}/api/v1/discount/scheme/${state.id}`,
-        values,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-      navigate(`/dashboard/discounts`);
-    } catch (error) {
-      console.log(error);
+
+  const handleSubmit = async (values: any) => {
+    setLoading(true);
+    const formData = getFormData(values);
+    const result: any = await editDiscount(token, state.id, formData);
+    if (result?.status === 200 || result?.statusText === "OK") {
+      setLoading(false);
+      navigate("/dashboard/discounts");
+    } else {
+      setError(true);
+      setLoading(false);
     }
   };
+
   return (
-    <div className="w-full space-y-5 md:pt-8 px-5">
+    <div className="w-full space-y-5 md:pt-8">
       <div>
         <h4 className="text-orange-400 font-semibold text-center text-2xl">
           Edit Discount Scheme
@@ -42,24 +45,34 @@ const EditDiscount = () => {
             notes: state.notes || "",
             enabled: state.enabled,
           }}
-          onSubmit={(values) => {
-            const formData = getFormData(values);
-            EditDiscount(formData);
+          validationSchema={validationSchema}
+          onSubmit={(values: any) => {
+            handleSubmit(values);
           }}
         >
           {({ values, handleChange }) => (
             <Form className=" space-y-5 block w-full">
-              <div className="p-5 bg-white rounded-lg shadow-lg w-5/6 xl:w-1/2 mx-auto space-y-5">
+              <div className="p-5 bg-white rounded-lg shadow-lg w-11/12 md:w-5/6 xl:w-1/2 mx-auto space-y-5">
                 <div className="flex items-center justify-between">
                   <p className="font-semibold text-gray-500">
                     Discount Scheme Information
                   </p>
+                  {error ? (
+                    <Toast
+                      message="cannot add new discount scheme now"
+                      close={() => {
+                        setError(false);
+                      }}
+                      show={error}
+                      type="error"
+                    />
+                  ) : null}
                   <div
                     className="border border-orange-500 rounded-full px-4 py-1 space-x-3 cursor-pointer"
                     onClick={() => {
                       const val = { ...values, enabled: !values.enabled };
                       const formData = getFormData(val);
-                      EditDiscount(formData);
+                      editDiscount(token, state.id, formData);
                     }}
                   >
                     <p className="text-sm  text-orange-500">
@@ -88,13 +101,14 @@ const EditDiscount = () => {
                   onChange={handleChange}
                   type="text"
                 />
-                <div className="flex justify-end">
+                <div className="flex justify-end items-center space-x-4">
                   <button
                     type="submit"
                     className="text-sm md:text-base block px-5 bg-orange-500 text-white py-1.5 rounded-full font-semibold"
                   >
                     Submit
                   </button>
+                  {loading ? <ButtonSpinner /> : null}
                 </div>
               </div>
             </Form>
@@ -106,3 +120,8 @@ const EditDiscount = () => {
 };
 
 export default EditDiscount;
+const validationSchema = yup.object().shape({
+  discountTitle: yup.string().required("Discount Title is required"),
+  discountValue: yup.string().required("Discount Value is required"),
+  notes: yup.string().required("Discount Note is required"),
+});
